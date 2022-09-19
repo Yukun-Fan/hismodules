a5c243deb892ab88bba28b6be28e69d426344784
Administrator <Administrator@PC-20180915YQGO>
2022-09-19 16:26:30 +0800

    后端项目初始化结束

---
+++ drugstage/src/main/java/com/neuedu/DrugStageApplication.java
@@ -0,0 +1,12 @@
+package com.neuedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugStageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrugStageApplication.class, args);
+	}
+}
